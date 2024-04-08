61ffbec3e55705dbbecc462e055868729e1ecc52
Matthew McMahon <msmcmahon01@gmail.com>
2024-04-08 16:51:03 -0700

    asciiArt.js fix

--- asciiArt.js
+++ asciiArt.js
@@ -1,19 +1,12 @@
 var figlet = require("figlet");
-var readline = require("readline");
 
-var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+var text = process.argv.slice(2).join(" ");
 
-rl.question("Please enter the text to be translated into Ascii Art: ", function(text) {
-	figlet(text, function(err, data) {
-		if (err) {
-			console.log("Something went wrong...");
-			console.dir(err);
-			return;
-		}
-		console.log(data);
-	});
-	rl.close();
+figlet(text, function(err, data) {
+	if (err) {
+		console.log("Something went wrong...");
+		console.dir(err);
+		return;
+	}
+	console.log(data);
 });
