0f251d442046ef7a21bf2484ad02087e3e436796
Matthew McMahon <msmcmahon01@gmail.com>
2024-04-08 16:23:16 -0700

    commit

--- asciiArt.js
+++ asciiArt.js
@@ -1,12 +1,19 @@
 var figlet = require("figlet");
+var readline = require("readline");
 
-figlet("Hello World!!", function (err, data) {
-	if (err) {
-		console.log("Something went wrong...");
-		console.dir(err);
-		return;
-	}
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
-	console.log(data);
-	}
-);
+rl.question("Please enter the text to be translated into Ascii Art: ", function(text) {
+	figlet(text, function(err, data) {
+		if (err) {
+			console.log("Something went wrong...");
+			console.dir(err);
+			return;
+		}
+		console.log(data);
+	});
+	rl.close
+});
